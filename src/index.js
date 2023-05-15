9344682da35dab5eeee8d5cd0f0821e6f402213a
gisa chris <gisachris24@gmail.com>
2023-05-15 14:48:51 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+import './style.css';
+import refreshImage from './refresh.png';
+import * as functionality from './modules/addRemove.js';
+import * as interactions from './modules/interaction.js';
+
+// selecting the header section
+const headerSection = document.querySelector('.header');
+
+// making the refresh button image
+const refresh = document.createElement('img');
+refresh.classList.add('refreshButton');
+refresh.src = refreshImage;
+
+headerSection.append(refresh);
+
+const refreshButton = document.querySelector('.refreshButton');
+refreshButton.addEventListener('click', () => {
+  if (refreshButton.classList.contains('rotate')) {
+    refreshButton.classList.toggle('rotate');
+    refreshButton.classList.add('rotateBack');
+  } else if (refreshButton.classList.contains('rotateBack')) {
+    refreshButton.classList.toggle('rotateBack');
+    refreshButton.classList.add('rotate');
+  } else {
+    refreshButton.classList.add('rotate');
+  }
+});
